5475415cd0293df4ed89dcb8bad2138ec514c1ed
Lala Qasimova <lalaqasim936@gmail.com>
2026-04-16 21:43:54 +0000

    Task 10: 10-matisse

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Module that computes derivative of a polynomial.
+"""
+
+
+def poly_derivative(poly):
+    """
+    Returns derivative of a polynomial represented as a list.
+
+    poly: list of coefficients
+    """
+
+    if (not isinstance(poly, list) or len(poly) == 0):
+        return None
+    
+    for c in poly:
+        if not isinstance(c, (int, float)):
+            return None
+    
+    result = []
+    for i in range(1, len(poly)):
+        result.append(poly[i] * i)
+    
+    if len(result) == 0:
+        return [0]
+
+    if all(x == 0 for x in result):
+        return [0]
+
+    return result
