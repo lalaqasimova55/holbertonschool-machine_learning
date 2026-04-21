b6fa4dd2f1913ec10d16f0a84680a42023ed5bee
Lala Qasimova <lalaqasim936>
2026-04-21 19:58:26 +0000

    Advanced task: 101-the_whole_barn

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
-"""Module that adds two matrices of any dimension"""
+"""Module that adds matrices of any dimension"""
 
 
 def add_matrices(mat1, mat2):
-    """Returns a new matrix resulting from element-wise addition"""
+    """Returns element-wise sum of two matrices or None if mismatch"""
 
+    # different types → invalid
     if type(mat1) != type(mat2):
         return None
 
+    # if both are lists → recurse
     if isinstance(mat1, list):
         if len(mat1) != len(mat2):
             return None
-        return [add_matrices(m1, m2) for m1, m2 in zip(mat1, mat2)]
 
+        result = []
+        for i in range(len(mat1)):
+            res = add_matrices(mat1[i], mat2[i])
+            if res is None:
+                return None
+            result.append(res)
+
+        return result
+
+    # base case (numbers)
     return mat1 + mat2
