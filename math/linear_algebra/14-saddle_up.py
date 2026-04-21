879bbb5c4f9f1fbd4f4b1a0555ca0a8a46aca1bc
Lala Qasimova <lalaqasim936>
2026-04-21 19:51:48 +0000

    Task 14: 14-saddle_up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that performs matrix multiplication using numpy"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Returns the matrix product of two numpy arrays"""
+    return np.matmul(mat1, mat2)
