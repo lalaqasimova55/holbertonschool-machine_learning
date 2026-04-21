74d4b73c5bada802af1008caeffb9836ea8fd992
Lala Qasimova <lalaqasim936>
2026-04-21 19:45:31 +0000

    Task 10: 10-ill_use_my_scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that returns the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy array as a tuple"""
+    return matrix.shape
