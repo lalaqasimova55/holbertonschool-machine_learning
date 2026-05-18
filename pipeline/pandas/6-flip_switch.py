509b33506063f7c1535d74b576c5881546269c1e
Lala Qasimova <lalaqasim936@gmail.com>
2026-05-18 22:46:53 +0000

    Task 6: 6-flip_switch

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -3,14 +3,9 @@
 Defines a function that flips and switches a DataFrame
 """
 
+
 def flip_switch(df):
     """
     Sorts data in reverse chronological order and transposes it
-
-    Args:
-        df (pd.DataFrame): input dataframe
-
-    Returns:
-        pd.DataFrame: transformed dataframe
     """
     return df.sort_index(ascending=False).T
