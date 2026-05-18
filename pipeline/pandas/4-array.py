03988d84d2cf98397ff1005f78012d2a12d2794f
Lala Qasimova <lalaqasim936@gmail.com>
2026-05-18 22:37:57 +0000

    Task 4: 4-array

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 """
-Defines a function to extract the last 10 rows of specific columns as a NumPy array
+Defines a function to extract the last 10 rows of specific columns as an array
 """
-import numpy as np
 
 
 def array(df):
     """
     Selects the last 10 rows of the 'High' and 'Close' columns from a DataFrame
-    and converts them into a numpy.ndarray.
+    and converts them into a numpy.ndarray without importing numpy.
 
     Args:
         df (pd.DataFrame): The input DataFrame.
 
     Returns:
-        np.ndarray: A 10x2 NumPy array containing the selected values.
+        numpy.ndarray: A 10x2 NumPy array containing the selected values.
     """
-    # Select the High and Close columns, grab the last 10 rows, and convert to numpy
     ndarray = df[['High', 'Close']].tail(10).to_numpy()
-    
     return ndarray
