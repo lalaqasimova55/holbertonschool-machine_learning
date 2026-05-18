08bdd80c01dcc23935f4d750dc1971030fefad4b
Lala Qasimova <lalaqasim936@gmail.com>
2026-05-18 22:55:08 +0000

    Task 11: 11-concat

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Defines a function that concatenates two DataFrames with keys
+"""
+
+
+index = __import__('10-index').index
+
+
+def concat(df1, df2):
+    """
+    Index both DataFrames on Timestamp and concatenate them
+
+    Args:
+        df1 (pd.DataFrame): coinbase dataframe
+        df2 (pd.DataFrame): bitstamp dataframe
+
+    Returns:
+        pd.DataFrame: concatenated dataframe
+    """
+
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2 = df2[df2.index <= 1417411920]
+
+    return pd.concat(
+        [df2, df1],
+        keys=['bitstamp', 'coinbase']
+    )
