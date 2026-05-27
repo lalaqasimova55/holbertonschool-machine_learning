dccad904f057a6af6b22e720c68ee2d9f7cfccf6
Lala Qasimova <lalaqasim936@gmail.com>
2026-05-27 19:13:59 +0000

    Task 0: 0-line

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,13 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
+    """Plot y = x^3 for x from 0 to 10."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    # Your code here
     x = np.arange(0, 11)
     plt.plot(x, y, color='red', linestyle='solid')
     plt.xlim(0, 10)
-    
+
     plt.show()
