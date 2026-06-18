e405c17c7464960341822bfa621ebccf4577f2a7
Lala Qasimova <lalaqasim936@gmail.com>
2026-06-18 21:54:18 +0000

    11-isolation_forest

---
+++ supervised_learning/decision_tree/11-isolation_forest.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Isolation Random Forest module for anomaly and outlier detection.
+"""
+
+import numpy as np
+Isolation_Random_Tree = __import__('10-isolation_tree').Isolation_Random_Tree
+
+
+class Isolation_Random_Forest():
+    """
+    Isolation Random Forest ensemble model.
+    """
+
+    def __init__(self, n_trees=100, max_depth=10, min_pop=1, seed=0):
+        """Initializes the isolation forest attributes."""
+        self.numpy_predicts = []
+        self.target = None
+        self.numpy_preds = None
+        self.n_trees = n_trees
+        self.max_depth = max_depth
+        self.seed = seed
+
+    def predict(self, explanatory):
+        """Computes the mean depth predictions across all isolation trees."""
+        predictions = np.array([f(explanatory) for f in self.numpy_preds])
+        return predictions.mean(axis=0)
+
+    def fit(self, explanatory, n_trees=100, verbose=0):
+        """Fits multiple isolation random trees to build the forest ensemble."""
+        self.explanatory = explanatory
+        self.numpy_preds = []
+        depths = []
+        nodes = []
+        leaves = []
+        for i in range(n_trees):
+            T = Isolation_Random_Tree(max_depth=self.max_depth,
+                                      seed=self.seed + i)
+            T.fit(explanatory)
+            self.numpy_preds.append(T.predict)
+            depths.append(T.depth())
+            nodes.append(T.count_nodes())
+            leaves.append(T.count_nodes(only_leaves=True))
+        if verbose == 1:
+            print(f"  Training finished.\n"
+                  f"    - Mean depth                     : "
+                  f"{np.array(depths).mean()}\n"
+                  f"    - Mean number of nodes           : "
+                  f"{np.array(nodes).mean()}\n"
+                  f"    - Mean number of leaves          : "
+                  f"{np.array(leaves).mean()}")
+
+    def suspects(self, explanatory, n_suspects):
+        """Returns the n_suspects rows that have the smallest mean depth."""
+        depths = self.predict(explanatory)
+
+        # Ortalama dərinliyi ən kiçikdən böyüyə doğru sıralayan indeksləri tapırıq
+        sorted_indices = np.argsort(depths)
+
+        # Ən kiçik n_suspects sayda indeksi seçirik
+        suspect_indices = sorted_indices[:n_suspects]
+
+        # Müvafiq sətirləri və onların dərinliklərini geri qaytarırıq
+        return explanatory[suspect_indices], depths[suspect_indices]
