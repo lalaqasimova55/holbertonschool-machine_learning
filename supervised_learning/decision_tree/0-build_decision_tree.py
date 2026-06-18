29f576b493526447a9878fd2cae9665305c46724
Lala Qasimova <lalaqasim936@gmail.com>
2026-06-18 20:37:10 +0000

    Task 0: 0-build_decision_tree

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
+
 class Node:
     def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
         self.feature = feature
@@ -13,14 +16,7 @@ class Node:
 
     def max_depth_below(self):
         """
-        Computes the maximum depth of the subtree rooted at this node.
-
-        The method recursively explores the left and right children (if they exist)
-        and returns the greatest depth found among all descendant nodes, including
-        the current node itself.
-
-        Returns:
-            int: The maximum depth value in the subtree.
+        Returns maximum depth in subtree rooted at this node.
         """
         if self.is_leaf or (self.left_child is None and self.right_child is None):
             return self.depth
@@ -29,3 +25,29 @@ class Node:
         right_depth = self.right_child.max_depth_below() if self.right_child else self.depth
 
         return max(self.depth, left_depth, right_depth)
+
+
+class Leaf(Node):
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        return self.depth
+
+
+class Decision_Tree:
+    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+        self.rng = np.random.default_rng(seed)
+        self.root = root if root else Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        return self.root.max_depth_below()
