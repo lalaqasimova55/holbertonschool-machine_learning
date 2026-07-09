fcbc86d0fed2a994ed97f808107ee61d3d95128d
Lala Qasimova <lalaqasim936@gmail.com>
2026-07-09 20:02:56 +0000

    Task 3: 3-neuron.py

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""Defines a single neuron performing binary classification."""
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron class."""
+
+    def __init__(self, nx):
+        """
+        Initialize the neuron.
+
+        Args:
+            nx (int): Number of input features.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron.
+
+        Args:
+            X (numpy.ndarray): Input data of shape (nx, m).
+
+        Returns:
+            numpy.ndarray: The activated output of the neuron.
+        """
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost using logistic regression.
+
+        Args:
+            Y (numpy.ndarray): Correct labels of shape (1, m).
+            A (numpy.ndarray): Activated output of shape (1, m).
+
+        Returns:
+            float: The logistic regression cost.
+        """
+        m = Y.shape[1]
+        cost = -np.sum(Y * np.log(A) +
+                       (1 - Y) * np.log(1.0000001 - A)) / m
+        return cost
