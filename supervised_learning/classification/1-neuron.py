450dbc6f9d58905695bf16c0030dbda3dd2585c3
Lala Qasimova <lalaqasim936@gmail.com>
2026-07-09 19:58:34 +0000

    Task 1: 1-neuron.py

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Defines a single neuron performing binary classification."""
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron class."""
+
+    def __init__(self, nx):
+        """
+        Initialize the neuron.
+
+        Args:
+            nx (int): Number of input features.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output."""
+        return self.__A
