81d2b5f8c450a94ddcddcaa2c0d8e672d668fe60
Lala Qasimova <lalaqasim936@gmail.com>
2026-07-20 13:12:37 +0000

    3-l2_reg_create_layer.py

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Creates a layer with L2 regularization."""
+
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Creates a TensorFlow layer that includes L2 regularization.
+
+    Args:
+        prev: Tensor containing the output of the previous layer.
+        n: Number of nodes in the layer.
+        activation: Activation function.
+        lambtha: L2 regularization parameter.
+
+    Returns:
+        The output tensor of the new layer.
+    """
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode='fan_avg'
+    )
+
+    regularizer = tf.keras.regularizers.L2(lambtha)
+
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer,
+        kernel_regularizer=regularizer
+    )
+
+    return layer(prev)
