471da22d4ae6a4cb60350dc611120a01f88302d1
Lala Qasimova <lalaqasim936@gmail.com>
2026-07-20 13:15:35 +0000

    4-dropout_forward_prop.py

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -8,16 +8,14 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     """
     Conducts forward propagation using Dropout.
 
-    Args:
+    Parameters:
         X: numpy.ndarray of shape (nx, m)
         weights: dictionary containing the weights and biases
         L: number of layers
-        keep_prob: probability of keeping a neuron active
+        keep_prob: probability that a node will be kept
 
     Returns:
-        Dictionary containing:
-            A0 ... AL : activations
-            D1 ... D(L-1) : dropout masks
+        cache: dictionary containing the activations and dropout masks
     """
     cache = {}
     cache["A0"] = X
@@ -25,23 +23,23 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     for i in range(1, L + 1):
         W = weights["W{}".format(i)]
         b = weights["b{}".format(i)]
-        A_prev = cache["A{}".format(i - 1)]
 
+        A_prev = cache["A{}".format(i - 1)]
         Z = np.matmul(W, A_prev) + b
 
         if i == L:
             # Softmax output layer
-            exp = np.exp(Z - np.max(Z, axis=0, keepdims=True))
-            A = exp / np.sum(exp, axis=0, keepdims=True)
-            cache["A{}".format(i)] = A
+            t = np.exp(Z)
+            cache["A{}".format(i)] = t / np.sum(t, axis=0, keepdims=True)
         else:
-            # Hidden layers (tanh)
+            # Hidden layer (tanh)
             A = np.tanh(Z)
 
             # Dropout mask
-            D = np.random.rand(*A.shape) < keep_prob
-            A = A * D
-            A = A / keep_prob
+            D = np.random.binomial(1, keep_prob, size=A.shape)
+
+            # Apply inverted dropout
+            A = (A * D) / keep_prob
 
             cache["D{}".format(i)] = D
             cache["A{}".format(i)] = A
