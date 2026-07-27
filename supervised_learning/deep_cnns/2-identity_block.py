25db3572c6212aff20db0f3b5dd133c94a1fed87
Lala Qasimova <lalaqasim936@gmail.com>
2026-07-27 13:44:32 +0000

    2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -3,27 +3,19 @@
 Identity block for a ResNet
 """
 
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
     """
-    Builds an identity block as described in
-    Deep Residual Learning for Image Recognition (2015)
-
-    Args:
-        A_prev: output from the previous layer
-        filters: tuple/list containing (F11, F3, F12)
-
-    Returns:
-        Activated output of the identity block
+    Builds an identity block
     """
     F11, F3, F12 = filters
+
     initializer = K.initializers.he_normal(seed=0)
 
-    # First component
     X = K.layers.Conv2D(
-        filters=F11,
+        F11,
         kernel_size=(1, 1),
         padding='same',
         kernel_initializer=initializer
@@ -31,9 +23,8 @@ def identity_block(A_prev, filters):
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
-    # Second component
     X = K.layers.Conv2D(
-        filters=F3,
+        F3,
         kernel_size=(3, 3),
         padding='same',
         kernel_initializer=initializer
@@ -41,16 +32,14 @@ def identity_block(A_prev, filters):
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
-    # Third component
     X = K.layers.Conv2D(
-        filters=F12,
+        F12,
         kernel_size=(1, 1),
         padding='same',
         kernel_initializer=initializer
     )(X)
     X = K.layers.BatchNormalization(axis=3)(X)
 
-    # Skip connection
     X = K.layers.Add()([X, A_prev])
     X = K.layers.Activation('relu')(X)
 
