0a146e3d02fba7c9f05947fb187a8d22e3b68e9e
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-17 14:55:49 +0000

    5-bayes_opt.py

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -65,7 +65,7 @@ class BayesianOptimization:
         for _ in range(iterations):
             X_next, _ = self.acquisition()
 
-            if np.any(np.isclose(self.gp.X, X_next)):
+            if np.any(np.all(np.isclose(self.gp.X, X_next), axis=1)):
                 break
 
             Y_next = self.f(X_next)
