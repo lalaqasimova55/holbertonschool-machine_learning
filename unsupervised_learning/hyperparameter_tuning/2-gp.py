772189e2d7a224527129673c2455dea11f5b3ac7
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-17 14:40:17 +0000

    2-gp.py

---
+++ unsupervised_learning/hyperparameter_tuning/2-gp.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Gaussian Process"""
+
+
+import numpy as np
+
+
+class GaussianProcess:
+    """Represents a noiseless 1D Gaussian process."""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """Initialize the Gaussian process."""
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """Calculate the covariance kernel matrix."""
+        return (self.sigma_f ** 2) * np.exp(
+            -((X1 - X2.T) ** 2) / (2 * self.l ** 2)
+        )
+
+    def predict(self, X_s):
+        """Predict the mean and variance of points in the GP."""
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(self.K)
+
+        mu = K_s.T @ K_inv @ self.Y
+        sigma = np.diag(K_ss - K_s.T @ K_inv @ K_s)
+
+        return mu.flatten(), sigma
+
+    def update(self, X_new, Y_new):
+        """Update the Gaussian process with a new sample."""
+        self.X = np.vstack((self.X, X_new))
+        self.Y = np.vstack((self.Y, Y_new))
+        self.K = self.kernel(self.X, self.X)
