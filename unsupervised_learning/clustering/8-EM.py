41fb7e5dc8152e8ebe2164275a275fd36ca33f30
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-21 20:33:17 +0000

    8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Expectation Maximization Module for GMM"""
 
-
 import numpy as np
 
 initialize = __import__('4-initialize').initialize
@@ -21,14 +20,15 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5,
         verbose: boolean, whether to print log likelihood
 
     Returns:
-        pi, m, S, g, l or None, None, None, None, None on failure
+        pi, m, S, g, l
+        or None, None, None, None, None on failure
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None, None, None
 
     n, d = X.shape
 
-    if not isinstance(k, int) or k <= 0 or k > n:
+    if not isinstance(k, int) or k <= 0 or k >= n:
         return None, None, None, None, None
 
     if not isinstance(iterations, int) or iterations <= 0:
@@ -41,31 +41,30 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5,
         return None, None, None, None, None
 
     pi, m, S = initialize(X, k)
+
     if pi is None or m is None or S is None:
         return None, None, None, None, None
 
-    l = 0
+    l = None
 
     for i in range(iterations):
-        g, l_new = expectation(X, pi, m, S)
-        if g is None or l_new is None:
-            return None, None, None, None, None
+        g, new_l = expectation(X, pi, m, S)
 
-        if abs(l_new - l) <= tol:
-            if verbose:
-                print("Log Likelihood after {} iterations: {:.5f}".format(
-                    i, l_new))
-            l = l_new
-            break
+        if g is None or new_l is None:
+            return None, None, None, None, None
 
         if verbose and (i % 10 == 0):
             print("Log Likelihood after {} iterations: {:.5f}".format(
-                i, l_new))
+                i, new_l))
+
+        if l is not None and abs(new_l - l) <= tol:
+            break
+
+        l = new_l
 
         pi, m, S = maximization(X, g)
+
         if pi is None or m is None or S is None:
             return None, None, None, None, None
-
-        l = l_new
 
     return pi, m, S, g, l
