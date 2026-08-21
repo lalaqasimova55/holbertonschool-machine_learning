78c8003a5641cfd0d0fcb392fa195536fd0c38a5
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-21 20:26:17 +0000

    8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -11,24 +11,13 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5,
                              verbose=False):
     """
     Performs the expectation maximization algorithm for a GMM.
-
-    Args:
-        X: numpy.ndarray of shape (n, d)
-        k: positive integer, number of clusters
-        iterations: positive integer, maximum number of iterations
-        tol: non-negative float, tolerance for early stopping
-        verbose: boolean, whether to print log likelihood
-
-    Returns:
-        pi, m, S, g, l
-        or None, None, None, None, None on failure
     """
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None, None, None
 
     n, d = X.shape
 
-    if n < 1 or d < 1:
+    if n == 0 or d == 0:
         return None, None, None, None, None
 
     if not isinstance(k, int) or k <= 0 or k > n:
@@ -48,31 +37,26 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5,
     if pi is None or m is None or S is None:
         return None, None, None, None, None
 
-    l = None
-    g = None
+    l = 0
 
     for i in range(iterations):
-        g, l_new = expectation(X, pi, m, S)
+        g, new_l = expectation(X, pi, m, S)
 
-        if g is None or l_new is None:
+        if g is None or new_l is None:
             return None, None, None, None, None
 
-        if verbose and (i % 10 == 0):
+        if verbose and i % 10 == 0:
             print("Log Likelihood after {} iterations: {:.5f}".format(
-                i, l_new))
-
-        if l is not None and abs(l_new - l) <= tol:
-            if verbose:
-                print("Log Likelihood after {} iterations: {:.5f}".format(
-                    i, l_new))
-            l = l_new
-            break
+                i, new_l))
 
         pi, m, S = maximization(X, g)
 
         if pi is None or m is None or S is None:
             return None, None, None, None, None
 
-        l = l_new
+        if i > 0 and abs(new_l - l) <= tol:
+            break
 
-    return pi, m, S, g, l
+        l = new_l
+
+    return pi, m, S, g, new_l
