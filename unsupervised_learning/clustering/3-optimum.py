c83da3a1e58232467d977652bc60a593313602d8
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-21 20:10:25 +0000

    3-optimum.py

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Optimum K Module"""
+
 
 import numpy as np
 
@@ -7,45 +9,49 @@ variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
-    """Tests for the optimum number of clusters by variance."""
+    """Tests for the optimum number of clusters by variance.
 
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    Args:
+        X: numpy.ndarray of shape (n, d)
+        kmin: minimum number of clusters
+        kmax: maximum number of clusters
+        iterations: maximum number of K-means iterations
+
+    Returns:
+        results, d_vars or None, None on failure
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
 
     n = X.shape[0]
 
-    if not isinstance(kmin, int) or kmin < 1 or kmin >= n:
+    if not isinstance(kmin, int) or kmin <= 0 or kmin >= n:
         return None, None
 
     if kmax is None:
-        kmax = n - 1
+        kmax = n
 
-    if not isinstance(kmax, int) or kmax <= kmin or kmax >= n:
+    if not isinstance(kmax, int) or kmax <= kmin or kmax > n:
         return None, None
 
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
     results = []
-    d_vars = []
-    first_var = None
+    variances = []
 
     for k in range(kmin, kmax + 1):
         C, clss = kmeans(X, k, iterations)
-
         if C is None or clss is None:
             return None, None
 
-        results.append((C, clss))
-
         var = variance(X, C)
-
         if var is None:
             return None, None
 
-        if first_var is None:
-            first_var = var
+        results.append((C, clss))
+        variances.append(var)
 
-        d_vars.append(first_var - var)
+    d_vars = [variances[0] - var for var in variances]
 
     return results, d_vars
