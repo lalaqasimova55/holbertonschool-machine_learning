9462ab60ade578e97888b25c674d0ec8abe0aeb9
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-21 21:26:04 +0000

    8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,72 +1,53 @@
 #!/usr/bin/env python3
-"""Expectation Maximization Module for GMM"""
-
-
+"""Performs the expectation maximization for a GMM"""
 import numpy as np
-
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
 
 
-def expectation_maximization(X, k, iterations=1000, tol=1e-5,
-                             verbose=False):
-    """Performs the expectation maximization algorithm for a GMM.
-
-    Args:
-        X: numpy.ndarray of shape (n, d)
-        k: positive integer, number of clusters
-        iterations: positive integer, maximum number of iterations
-        tol: non-negative float, tolerance for early stopping
-        verbose: boolean, whether to print log likelihood
-
-    Returns:
-        pi, m, S, g, l or None, None, None, None, None on failure
-    """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
+    """Performs the expectation maximization for a GMM"""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None, None, None
-
-    n, d = X.shape
-
-    if not isinstance(k, int) or k <= 0 or k > n:
+    if type(k) is not int or k <= 0:
         return None, None, None, None, None
-
-    if not isinstance(iterations, int) or iterations <= 0:
+    if type(iterations) is not int or iterations <= 0:
         return None, None, None, None, None
-
-    if not isinstance(tol, (int, float)) or tol < 0:
+    if type(tol) is not float or tol < 0:
         return None, None, None, None, None
-
-    if not isinstance(verbose, bool):
+    if type(verbose) is not bool:
         return None, None, None, None, None
 
     pi, m, S = initialize(X, k)
     if pi is None or m is None or S is None:
         return None, None, None, None, None
 
-    l = None
+    g, lkhd_prev = expectation(X, pi, m, S)
+    if g is None or lkhd_prev is None:
+        return None, None, None, None, None
 
     for i in range(iterations):
-        g, l_new = expectation(X, pi, m, S)
-        if g is None or l_new is None:
-            return None, None, None, None, None
-
-        # Tolerance yoxlaması
-        if l is not None and abs(l_new - l) <= tol:
-            if verbose:
-                print("Log Likelihood after {} iterations: {:.5f}".format(
-                    i, l_new))
-            l = l_new
-            break
-
-        if verbose and (i % 10 == 0):
-            print("Log Likelihood after {} iterations: {:.5f}".format(
-                i, l_new))
+        if verbose and i % 10 == 0:
+            print('Log Likelihood after {} iterations: {}'.format(
+                i, round(lkhd_prev, 5)))
 
         pi, m, S = maximization(X, g)
         if pi is None or m is None or S is None:
             return None, None, None, None, None
 
-        l = l_new
+        g, lkhd = expectation(X, pi, m, S)
+        if g is None or lkhd is None:
+            return None, None, None, None, None
 
-    return pi, m, S, g, l
+        if np.abs(lkhd - lkhd_prev) <= tol:
+            break
+
+        lkhd_prev = lkhd
+
+    i += 1
+    if verbose:
+        print('Log Likelihood after {} iterations: {}'.format(
+            i, round(lkhd, 5)))
+
+    return pi, m, S, g, lkhd
