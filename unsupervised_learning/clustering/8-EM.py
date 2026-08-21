11c6d0a860469ac96493ff96eda4d5eca4ea719b
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-21 20:43:25 +0000

    8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -15,20 +15,23 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5,
 
     Args:
         X: numpy.ndarray of shape (n, d)
-        k: positive integer, number of clusters
-        iterations: positive integer, maximum number of iterations
-        tol: non-negative float, tolerance for early stopping
-        verbose: boolean, whether to print log likelihood
+        k: positive integer containing the number of clusters
+        iterations: positive integer containing the maximum number
+            of iterations
+        tol: non-negative float containing tolerance of the log
+            likelihood
+        verbose: boolean that determines if information is printed
 
     Returns:
-        pi, m, S, g, l or None, None, None, None, None on failure
+        pi, m, S, g, l
+        or None, None, None, None, None on failure
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None, None, None
 
     n, d = X.shape
 
-    if not isinstance(k, int) or k <= 0 or k > n:
+    if not isinstance(k, int) or k <= 0 or k >= n:
         return None, None, None, None, None
 
     if not isinstance(iterations, int) or iterations <= 0:
@@ -41,29 +44,27 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5,
         return None, None, None, None, None
 
     pi, m, S = initialize(X, k)
+
     if pi is None or m is None or S is None:
         return None, None, None, None, None
 
-    l = None
+    l = 0
 
     for i in range(iterations):
         g, l_new = expectation(X, pi, m, S)
+
         if g is None or l_new is None:
             return None, None, None, None, None
 
-        # Tolerance yoxlaması
-        if l is not None and abs(l_new - l) <= tol:
-            if verbose:
-                print("Log Likelihood after {} iterations: {:.5f}".format(
-                    i, l_new))
-            l = l_new
-            break
-
-        if verbose and (i % 10 == 0):
+        if verbose and i % 10 == 0:
             print("Log Likelihood after {} iterations: {:.5f}".format(
                 i, l_new))
 
+        if i > 0 and abs(l_new - l) <= tol:
+            break
+
         pi, m, S = maximization(X, g)
+
         if pi is None or m is None or S is None:
             return None, None, None, None, None
 
