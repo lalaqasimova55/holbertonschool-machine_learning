275933635254a99ed0b63e267feed034b65958f5
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-30 16:48:51 +0000

    2-word2vec.py

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Creates and trains a Word2Vec model."""
 
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
@@ -22,7 +22,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     Returns:
         The trained Word2Vec model
     """
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         vector_size=vector_size,
         min_count=min_count,
         window=window,
@@ -33,6 +33,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     )
 
     model.build_vocab(sentences)
+
     model.train(
         sentences,
         total_examples=model.corpus_count,
