abd2226fe1be6d8cabb4cc9cd97334ac6578a2b6
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-30 17:09:22 +0000

    2-word2vec.py

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -32,14 +32,9 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         window=window,
         negative=negative,
         sg=sg,
+        epochs=epochs,
         seed=seed,
         workers=workers,
-    )
-
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=epochs,
     )
 
     return model
