4ff74471d0fb36ec9b7c3d9a87dd9512a54d2ac8
Lala Qasimova <lalaqasim@gmail.com>
2026-08-31 16:03:31 +0000

    0-monte_carlo.py

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -32,7 +32,7 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0
                 break
             state = next_state
 
-        # First-visit Monte Carlo
+        # First-visit Monte Carlo update (geriyə doğru hesablanma)
         visited_states = set()
         G = 0
         for state, reward in reversed(episode_data):
