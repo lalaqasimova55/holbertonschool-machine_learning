c1a2909a5d6d40f54c0d962b956df30e86f83d11
Lala Qasimova <lalaqasim936@gmail.com>
2026-08-31 18:32:03 +0000

    4-fasttext.py

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -9,7 +9,8 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
 
     Returns: the trained model
     """
-    model = gensim.models.FastText(vector_size=vector_size,
+    model = gensim.models.FastText(sentences=sentences,
+                                   vector_size=vector_size,
                                    min_count=min_count,
                                    negative=negative,
                                    window=window,
@@ -17,8 +18,4 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
                                    epochs=epochs,
                                    seed=seed,
                                    workers=workers)
-    model.build_vocab(sentences)
-    model.train(sentences,
-                total_examples=model.corpus_count,
-                epochs=model.epochs)
     return model
